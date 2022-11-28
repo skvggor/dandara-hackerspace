7bb68ad33b217e602e65aba3100a366e44076eed
Marcos Garcia de Lima <marckfree@gmail.com>
2022-11-28 19:59:34 -0300

    Add form sender with firebase

---
+++ lead/components/PageGenerator/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="components">
+    <div
+      v-for="component in data"
+      :key="component.id"
+      class="component"
+    >
+      <LeadComponent
+        v-if="component.name === 'lead'"
+        :data="component"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import LeadComponent from '~/components/Lead'
+
+export default {
+  name: 'PageGenerator',
+  components: {
+    LeadComponent,
+  },
+  props: {
+    data: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
--- lead/nuxt.config.js
+++ lead/nuxt.config.js
@@ -1,42 +1,33 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
+  publicRuntimeConfig: {
+    PROJECT_ID: process.env.PROJECT_ID
+  },
   target: 'static',
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'lead',
+    title: 'Hackerspace Dandara',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'pt-br'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Barlow:wght@400;700;900&display=swap' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/stylelint
-    '@nuxtjs/stylelint-module',
+    '@nuxtjs/stylelint-module'
   ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {}
 }
